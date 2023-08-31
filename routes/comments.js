67919d37252fbc66fe944d90559347e0e0ea26d7
marceloiuli <marceloiuli26@gmail.com>
2023-08-31 09:09:16 -0700

    ensured log in for routes

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const commentsCtrl = require('../controllers/comments');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 //All routes start with '/' (root)
-router.post('/posts/:id/comments', commentsCtrl.create);
+router.post('/posts/:id/comments', ensureLoggedIn, commentsCtrl.create);
 
 router.delete('/comments/:id', commentsCtrl.delete);
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var postsCtrl = require('../controllers/posts')
+var postsCtrl = require('../controllers/posts');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 // All actual paths start with "/posts"
 
 router.get('/', postsCtrl.index)
 
-router.get('/new', postsCtrl.new)
+router.get('/new', ensureLoggedIn, postsCtrl.new)
 
-router.post('/', postsCtrl.create)
+router.post('/', ensureLoggedIn, postsCtrl.create)
 
-router.get('/:id', postsCtrl.show)
+router.get('/:id', ensureLoggedIn, postsCtrl.show)
 
 router.delete('/:id', postsCtrl.delete)
 
-router.get('/:id/edit', postsCtrl.edit)
+router.get('/:id/edit', ensureLoggedIn, postsCtrl.edit)
 
 router.put('/:id', postsCtrl.update)
 
